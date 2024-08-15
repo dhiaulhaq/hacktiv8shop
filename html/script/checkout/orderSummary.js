de2339ff9249d89323e2765cdeae635078103712
dhiaulhaq <dmnaufal7@gmail.com>
2024-08-15 17:29:14 +0700

    feat: delete data and calcucalte price

--- html/script/checkout/orderSummary.js
+++ html/script/checkout/orderSummary.js
@@ -1,42 +1,56 @@
-import cart from "../data/cart.js";
+import {cart, removeFromCart} from "../data/cart.js";
 import {getProduct} from "../data/product.js"
 
-let cartSummary = '';
 
-export function renderOrderSummary(params) {
-    
+
+export function renderOrderSummary() {
+    let cartSummary = '';
+
     cart.forEach(cartItem => {
         let cartItemId = cartItem.productId;
-        let matchingProduct = getProduct(cartItemId);
-        console.log(matchingProduct);
+        const matchingProduct = getProduct(cartItemId);
+
         
-        // cartSummary += `<div class="cart-item-container">
-        //             <div class="cart-item-details-grid">
-        //               <img class="product-image" src="${matchingProduct.image}" />
-        //               <div class="cart-item-details">
-        //                 <div class="row" style="display: flex; justify-content: space-between;">
-        //                   <div class="product-name">
-        //                   ${matchingProduct.name}
-        //                   </div>
-        //                   <div class="product-price">$${matchingProduct.priceCents}</div>
-        //                 </div>
-        //                 <div class="product-quantity">
-        //                   <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
-        //                   <span class="update-quantity-link link-primary">
-        //                     Update
-        //                   </span>
-        //                   <span class="delete-quantity-link link-primary">
-        //                     Delete
-        //                   </span>
-        //                 </div>
-        //               </div>
-        //             </div>
-        //           </div>`
+        cartSummary += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+                    <div class="cart-item-details-grid">
+                      <img class="product-image" src="${matchingProduct.image}" />
+                      <div class="cart-item-details">
+                        <div class="row" style="display: flex; justify-content: space-between;">
+                          <div class="product-name">
+                          ${matchingProduct.name}
+                          </div>
+                          <div class="product-price">$${matchingProduct.priceCents}</div>
+                        </div>
+                        <div class="product-quantity">
+                          <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
+                          <span class="update-quantity-link link-primary">
+                            Update
+                          </span>
+                          <span class="delete-quantity-link link-primary-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
+                            Delete
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`;
+    });
+    const orderSummary = document.querySelector(".order-summary");
+    orderSummary.innerHTML = cartSummary;
+    
+    const deleteOrder = document.querySelectorAll(".delete-quantity-link");
+    
+    deleteOrder.forEach(link => {
+        link.addEventListener('click',()=>{
+            
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+            // const container = document.querySelector(`js-cart-item-container-${matchingProduct.id}`);
+            // container.remove();
+            renderOrderSummary();
+        });
+        
     });
 }
-
-const orderSummary = document.querySelector(".order-summary");
-orderSummary.innerHTML = cartSummary;
 
 
 
